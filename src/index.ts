3bd1166f8b1eb9b668455213cc6b073b113f5fa4
Sandip <sandipramanik07@gmail.com>
2024-07-16 10:57:26 +0530

    feat: create sudoku based on difficulty with dotprompt

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,43 @@
 import * as z from "zod";
 
-// Import the Genkit core libraries and plugins.
-import { generate } from "@genkit-ai/ai";
 import { configureGenkit } from "@genkit-ai/core";
 import { defineFlow, startFlowsServer } from "@genkit-ai/flow";
 
-// Import models from the Google AI plugin. The Google AI API provides access to
-// several generative models. Here, we import Gemini 1.5 Flash.
-import { googleAI, gemini15Flash } from "@genkit-ai/googleai";
+import { googleAI } from "@genkit-ai/googleai";
+import { CreateSudokuSchema } from "./schemas/create-sudoku-schema";
+import { dotprompt, prompt } from "@genkit-ai/dotprompt";
 
 configureGenkit({
-  plugins: [
-    // Load the Google AI plugin. You can optionally specify your API key
-    // by passing in a config object; if you don't, the Google AI plugin uses
-    // the value from the GOOGLE_GENAI_API_KEY environment variable, which is
-    // the recommended practice.
-    googleAI(),
-  ],
-  // Log debug output to tbe console.
+  plugins: [googleAI(), dotprompt()],
   logLevel: "debug",
-  // Perform OpenTelemetry instrumentation and enable trace collection.
   enableTracingAndMetrics: true,
 });
 
-// Define a simple flow that prompts an LLM to generate menu suggestions.
-const menuSuggestionFlow = defineFlow(
-  {
-    name: "menuSuggestionFlow",
-    inputSchema: z.string(),
-    outputSchema: z.string(),
-  },
-  async (subject) => {
-    // Construct a request and send it to the model API.
-    const llmResponse = await generate({
-      prompt: `Suggest an item for the menu of a ${subject} themed restaurant`,
-      model: gemini15Flash,
-      config: {
-        temperature: 1,
-      },
-    });
+const difficultySchema = z.object({
+  difficulty: z.string(),
+});
 
-    // Handle the response from the model API. In this sample, we just convert
-    // it to a string, but more complicated flows might coerce the response into
-    // structured output or chain the response into another LLM call, etc.
-    return llmResponse.text();
+const createSudokuFlow = defineFlow(
+  {
+    name: "createSudokuFlow",
+    inputSchema: difficultySchema,
+    outputSchema: CreateSudokuSchema,
+  },
+  async (input) => {
+    const dPrompt = await prompt<z.infer<typeof difficultySchema>>(
+      "create-sudoku"
+    );
+    const result = await dPrompt.generate({ input });
+    const response: z.infer<typeof CreateSudokuSchema> = result.output();
+
+    const c = response.puzzle.flat().filter((e) => e === -1).length;
+    console.log(`********** Empty cells count: ${c}`);
+
+    return response;
   }
 );
 
-// Start a flow server, which exposes your flows as HTTP endpoints. This call
-// must come last, after all of your plug-in configuration and flow definitions.
-// You can optionally specify a subset of flows to serve, and configure some
-// HTTP server options, but by default, the flow server serves all defined flows.
 startFlowsServer({
   port: parseInt(process.env.PORT || "3400"),
-  flows: [menuSuggestionFlow],
+  flows: [createSudokuFlow],
 });
---
+++ src/schemas/create-sudoku-schema.ts
@@ -0,0 +1,35 @@
+import { defineSchema } from "@genkit-ai/core";
+import * as z from "zod";
+
+// ------ FOR SOLUTION SCHEMA ------------
+const sNumberInRange = z.number().gte(1).lte(9).nonnegative();
+const sRowWithNumberInRange = sNumberInRange.array().length(9);
+
+const sValidRow = sRowWithNumberInRange.refine(
+  (items) => new Set(items).size === items.length,
+  { message: "Must be an array of unique numbers" }
+);
+
+const solutionSudokuSchema = z.array(sValidRow).length(9);
+
+// ------ FOR PUZZLE SCHEMA ------------
+const pNumberInRange = z.union([sNumberInRange, z.literal(-1)]);
+const pRowWithNumberInRange = pNumberInRange.array().length(9);
+
+const pValidRow = pRowWithNumberInRange.refine(
+  (items) => {
+    const positiveNumberArray = items.filter((item) => item > 0);
+    return new Set(positiveNumberArray).size === positiveNumberArray.length;
+  },
+  { message: "Must be an array of unique positive numbers" }
+);
+
+const puzzleSudokuSchema = z.array(pValidRow).length(9);
+
+export const CreateSudokuSchema = defineSchema(
+  "CreateSudokuSchema",
+  z.object({
+    puzzle: puzzleSudokuSchema,
+    solution: solutionSudokuSchema,
+  })
+);
